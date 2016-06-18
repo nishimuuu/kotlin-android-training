861bec76aa124bb292ee19826257f77000933a2b
Takahiro Nishimura <takahiro_nishimura@r.recruit.co.jp>
2016-06-18 16:36:44 +0900

    convert java file to kotlin

---
+++ app/src/androidTest/java/jp/ac/u/tokyo/takahiro/myapplication2/app/ApplicationTest.kt
@@ -0,0 +1,9 @@
+package jp.ac.u.tokyo.takahiro.myapplication2.app
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+/**
+ * [Testing Fundamentals](http://d.android.com/tools/testing/testing_android.html)
+ */
+class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
